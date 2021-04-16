3978439beb197a97e56059af15c94c58245e0425
Emma Spindler <ejspindler7@gmail.com>
2021-04-16 15:48:06 -0500

    rough draft of team documentation

--- project/include/mainpage.h
+++ project/include/mainpage.h
@@ -131,4 +131,60 @@ applications where extensibility is not a major concern, the concrete factory
 pattern, or even a simple factory method are likely better options than the
 composite factory pattern.
 
+
+In your Doxygen mainpage add a section named: 
+Team Documentation that includes summaries of team meetings
+ (minimum of three) and who was assigned a task to complete.
+
+* \section Team Meetings
+*  All team mebers engaged in each and every meeting. 
+
+Iteration 2 First Deliverable 
+March 26, 2021
+ Meeting one consisted of Reading the Iteration 2 requirements and having an understanding 
+ of the the requiements for the first delvierable. This meeting also consisted of making the decision to use
+ Anthony's code from Iteration 1. Anthony pushed his code to the shared repo for our team. Planned for next meeting on Monday. 
+
+ Anthony was assigned Priroity level 1 number 2. This consisted of created a robot class and restructuring the code then to have 
+ a courier class. The robot and drone class inherit from this courier class. Anthony already had the capability of multiple drones and 
+ packages being delivered from multiple Drone/Robots because he completed the extra credit from Iteration 1. This satifies Priority level 2 number 1. 
+
+ Tia was assigned to fix the google tests from Iteration 1. This was Priority level 1 number 1.
+
+ Imran was assigned writing google tests for the Observer pattern.
+
+ Emma was assigned the Observer, priority level 2 number 2 and 3. Emma was responsible for getting the observer to report when the packages are scheduled, picked up, and dropped off.
+ Emma was also responsible for getting the observer to report for Drone/Robot's entering idle state and entering moving state.
+
+
+March 29, 2021
+Meeting two consisted of updated each other on where we are at with our assigned work and to look over the grading rubric for this deliverable. Discussed if there needs to be 
+any modifcations on the distributation of work. As google tests for the observer are no longer a part of the first deliverable Imrans tasks need to be altered. Planned for next meeting on Tuesday after spring break.
+
+Imran was unassigned writing google tests for the observer and instead is now repsonsible for priority level 2 number 3. Imran is responsible for getting the observer to report 
+for Drone/Robot's entering idle state and entering moving state.
+
+Emma was unassigned priority level 2 number 3, the observer pattern for the Drone/Robot.
+
+Tia was assigned gogole tests for the rafactored code from Drone, Robot, courier, and corresponding factory classes.
+
+
+Iteration 2 Final Deliverable
+April 13, 2021 
+Meeting three consisted of understanding the requirments for this deliverable and distributing the work between the team. Planned for a follow up meeting to occur on Friday at 3 pm. 
+
+Anthony was assigned priority level 3 which consists implementing a route strategy. Anthony is also repsonsible for documenting the deisgn and implementation.
+
+Tia was assigned the documentation of all new classes and methods are documented and updated the UML according to the changes made.
+
+Imran was assigned priority level 4 which consists of handling the case when the battery runs out and the need to reschedule the delivery. 
+
+Emma was assign the documentation of the Team Meetings.
+
+
+April 16, 2021
+Discussed the distribution of the tasks to ensure it felt equal. Went over the grading rubric to ensure all tasks were accounted for. Discussed if everyone felt they would be able to complete their tasks before the dealine. Planned next meeting for Monday at 4 pm. 
+
+Emma was assigned the documentation for the Observer Pattern. 
+
 */
